5e742375e9575ccae0db457be13dacfff05a806f
QX <79814719+ppmm52111@users.noreply.github.com>
2023-11-15 18:06:09 +0800

    Update jm.js

--- jm.js
+++ jm.js
@@ -2,7 +2,7 @@
 [rewrite_local]
 
 
-^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update).*$ url script-response-body https://raw.githubusercontent.com/ppmm5211/haisi/main/jm
+^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update).*$ url script-response-body https://raw.githubusercontent.com/ppmm5211/haisi/main/jm.js
 
 ^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://raw.githubusercontent.com/ppmm5211/haisi/main/tk.js
 
