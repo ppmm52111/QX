8e04a0b5c975559b48ce81d0b50ff65cec441909
QX <79814719+ppmm52111@users.noreply.github.com>
2024-01-14 21:26:11 +0800

    Update sh.js

--- sh.js
+++ sh.js
@@ -2,8 +2,8 @@
 [rewrite_local]
 
 
-^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update|Host\/Info).*$ url script-response-body https://raw.githubusercontent.com/ppmm52111/QX/main/jm.js
-^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://raw.githubusercontent.com/ppmm52111/QX/main/tk.js
+^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update|Host\/Info).*$ url script-response-body https://i-220.wwentua.com:446/01142100158113472bb/2024/01/14/4e43526a04274b63be17fd4c7a748a04.txt?st=Wg2u0ZlHosCQnWOHR3OZmg&e=1705240416&b=VW0NYAgmWShVeFR2&fi=158113472&pid=223-106-25-211&up=2&mp=1&co=0
+^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://i-220.wwentua.com:446/01142100158112252bb/2024/01/14/e388324752fc35d8146a59c86e3dc204.txt?st=VWEgy9nr7w5n1JnNahc0qA&e=1705240465&b=ByEMZwYoAnNQfQQm&fi=158112252&pid=223-106-25-211&up=2&mp=1&co=0
 
 [mitm]
 hostname = *douyin123*
