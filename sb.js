827cfadee46e225a6c38319deb3cc3a57184954c
QX <79814719+ppmm52111@users.noreply.github.com>
2024-01-27 19:52:15 +0800

    Update sb.js

--- sb.js
+++ sb.js
@@ -2,8 +2,8 @@
 [rewrite_local]
 
 
-^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update|Host\/Info).*$ url script-response-body https://i-320.wwentua.com:446/01240800158440043bb/2024/01/16/577cee52a09db3519313cde7b7e1e281.txt?st=PIk2IxxFdol4_vi2sa02WA&e=1706059210&b=ADhaNwAuAHFYdV58&fi=158440043&pid=112-86-84-251&up=2&mp=1&co=0
-^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://i-620.wwentua.com:446/01240800158440046bb/2024/01/16/38e1439323e735649ee373882f949fa9.txt?st=ziSfxlTTvvGsJJo285G6hw&e=1706059225&b=VXMBalR6WClVeFFz&fi=158440046&pid=112-86-84-251&up=2&mp=1&co=0
+^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update|Host\/Info).*$ url script-response-body https://i-320.wwentua.com:446/01271900158440043bb/2024/01/16/577cee52a09db3519313cde7b7e1e281.txt?st=9Ng4doFs_Z2AzPqij7-V7w&e=1706358281&b=AzsOY1N9BHUFKAQm&fi=158440043&pid=223-106-25-211&up=2&mp=1&co=0
+^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://i-620.wwentua.com:446/01271900158440046bb/2024/01/16/38e1439323e735649ee373882f949fa9.txt?st=3ccH6FCshjykvhLPUv9m1w&e=1706358294&b=U3UJYlB_bAHFRfAcl&fi=158440046&pid=223-106-25-211&up=2&mp=1&co=0
 [mitm]
 hostname = *douyin123*
 
