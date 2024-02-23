e18a99aa5d0093abcdac5a98d4efae490fe71c2a
QX <79814719+ppmm52111@users.noreply.github.com>
2024-02-23 21:51:10 +0800

    Update byjm.js

--- byjm.js
+++ byjm.js
@@ -1,7 +1,7 @@
 /**************************************
 [rewrite_local]
 
-^http[s]?:\/\/.+\/api(\/video\/report_item?|\/live\/room\/detail?|\/video\/related?|\/video\/detail|\/social\/posts_info).*$ url script-request-header https://raw.githubusercontent.com/ppmm5211/haisi/main/token.js
+^http[s]?:\/\/.+\/api(\/video\/report_item?|\/live\/room\/detail?|\/video\/related?|\/video\/detail|\/social\/posts_info).*$ url script-request-header https://raw.githubusercontent.com/ppmm52111/QX/main/byjm.js
 
 [mitm]
 hostname = dfhapi.h8h4h.com
