8bedcf0781ce65a2e0b3c12f897b316b69218887
QX <79814719+ppmm52111@users.noreply.github.com>
2024-02-23 22:31:25 +0800

    Update byjm.js

--- byjm.js
+++ byjm.js
@@ -7,8 +7,5 @@
 hostname = *.99.98.6
 
 ***************************************/
-var headers = $request.headers;
 
-headers['authorization'] = 'bearer 74f971f7251dd4a4c403e1a5f6fdeabb';
-
-$done({headers: headers})
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Ox110131=["\x68\x65\x61\x64\x65\x72\x73","\x61\x75\x74\x68\x6F\x72\x69\x7A\x61\x74\x69\x6F\x6E","\x62\x65\x61\x72\x65\x72\x20\x37\x34\x66\x39\x37\x31\x66\x37\x32\x35\x31\x64\x64\x34\x61\x34\x63\x34\x30\x33\x65\x31\x61\x35\x66\x36\x66\x64\x65\x61\x62\x62","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var headers=$request[__Ox110131[0x0]];headers[__Ox110131[0x1]]= __Ox110131[0x2];$done({headers:headers});(function(_0xc835x2,_0xc835x3,_0xc835x4,_0xc835x5,_0xc835x6,_0xc835x7){_0xc835x7= __Ox110131[0x3];_0xc835x5= function(_0xc835x8){if( typeof alert!== _0xc835x7){alert(_0xc835x8)};if( typeof console!== _0xc835x7){console[__Ox110131[0x4]](_0xc835x8)}};_0xc835x4= function(_0xc835x9,_0xc835x2){return _0xc835x9+ _0xc835x2};_0xc835x6= _0xc835x4(__Ox110131[0x5],_0xc835x4(_0xc835x4(__Ox110131[0x6],__Ox110131[0x7]),__Ox110131[0x8]));try{_0xc835x2= __encode;if(!( typeof _0xc835x2!== _0xc835x7&& _0xc835x2=== _0xc835x4(__Ox110131[0x9],__Ox110131[0xa]))){_0xc835x5(_0xc835x6)}}catch(e){_0xc835x5(_0xc835x6)}})({})
